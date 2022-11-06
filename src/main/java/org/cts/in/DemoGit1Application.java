b228c53ba3dc1624ac255fe88590474b55cdead4
sreevani8 <sreevanidumpala@gmail.com>
2022-11-06 13:19:02 +0530

    First Commit.

---
+++ src/main/java/org/cts/in/DemoGit1Application.java
@@ -0,0 +1,13 @@
+package org.cts.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGit1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/cts/in/DemoGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cts.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
